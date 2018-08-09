6e283568e07eff1b8a5e568cf61c2de3c13b7c16
yoadwo <yoadw20@gmail.com>
2018-08-09 13:07:23 +0300

    Add Scan Activity (working)

--- app/src/main/java/com/gingos/redpillandroid/MainActivity.java
+++ app/src/main/java/com/gingos/redpillandroid/MainActivity.java
@@ -1,13 +1,80 @@
 package com.gingos.redpillandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.barcode.Barcode;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG_main = "RedPill_MainActivity";
+
+    private static final int BARCODE_REQUEST_CODE = 100;
+    private static final int BARCODE_RESULT_OK_CODE = 101;
+    private static final int BARCODE_RESULT_ERROR_CODE = 102;
+
+    Button btn_scan;
+    TextView txt_scanText;
+    Barcode barcode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG_main, "onCreate:");
+        // REGISTER VIEWS
+
+        // scan button
+        btn_scan = findViewById(R.id.btn_scan);
+        btn_scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent scanIntent = new Intent(MainActivity.this, ScanActivity.class);
+                startActivityForResult(scanIntent, BARCODE_REQUEST_CODE);
+            }
+        });
+
+        // scan result text
+        txt_scanText = findViewById(R.id.txt_scanText);
+    }
+
+    // Listener for all returning activity request results
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case BARCODE_REQUEST_CODE:
+                Log.d(TAG_main, "onActivityResult: BARCODE_REQUEST");
+                if (resultCode == RESULT_OK){
+                    Log.d(TAG_main, "onActivityResult: BARCODE_RESULT_OK");
+                    if (data !=null){
+                        barcode = data.getParcelableExtra("barcode");
+                        Log.d(TAG_main, "onActivityResult: barcode raw: \n" + barcode.displayValue);
+                        // validate Pill protocol
+                        if (!barcode.displayValue.startsWith("FSPILLSEN@"))
+                            Toast.makeText(getApplicationContext(), "Sorry, Illegal text format", Toast.LENGTH_LONG).show();
+                        else {
+                            txt_scanText.setText(barcode.displayValue);
+                            //ParseTextFromQR(txt_scanText.toString());
+                        }
+                    }
+                    else{
+                        // data == null
+                        Toast.makeText(getApplicationContext(), "Sorry, data returned from camera scan is null", Toast.LENGTH_LONG).show();
+                        txt_scanText.setText("No barcode found");
+                    }
+                }
+                else if (resultCode == RESULT_CANCELED){
+                    Toast.makeText(getApplicationContext(), "Sorry, result Code is not OK", Toast.LENGTH_LONG).show();
+                }
+            break;
+            default:
+                super.onActivityResult(requestCode,resultCode, data);
+        }
     }
 }
---
+++ app/src/main/java/com/gingos/redpillandroid/ScanActivity.java
@@ -0,0 +1,129 @@
+package com.gingos.redpillandroid;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+public class ScanActivity extends AppCompatActivity {
+
+    private static final String TAG_scan = "RedPill_ScanActivity";
+    private static final int CAMERA_PERMISSION_RECQUEST_CODE = 200;
+
+    SurfaceView cameraView;
+    SurfaceHolder holder;
+    CameraSource cameraSource;
+    BarcodeDetector barcode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+        Log.d(TAG_scan, "onCreate: ");
+
+        // REGISTER VIEWS
+        // register camera
+        cameraView = findViewById(R.id.cameraView);
+        cameraView.setZOrderMediaOverlay(true);
+        holder = cameraView.getHolder();
+
+        // register barcode
+        barcode = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.QR_CODE)
+                .build();
+        // close activity (camera) if cannot init barcode
+        if(!barcode.isOperational()){
+            Toast.makeText(getApplicationContext(), "Sorry, Couldn't setup the detector", Toast.LENGTH_LONG).show();
+            this.finish();
+        }
+
+        // set camera view
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            //When, in the first instance, the surface is created, this method is called.
+            public void surfaceCreated(SurfaceHolder holder) {
+                try{
+                    Log.d(TAG_scan, "surfaceCreated: ");
+                    if(ContextCompat.checkSelfPermission(ScanActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+                        cameraSource.start(cameraView.getHolder());
+                    }else{
+                        Log.d(TAG_scan, "surfaceCreated: no permission");
+                        ActivityCompat.requestPermissions(ScanActivity.this, new String[] {Manifest.permission.CAMERA}, CAMERA_PERMISSION_RECQUEST_CODE);
+                    }
+
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            // This method is called when the size or the format of the surface changes.
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                Log.d(TAG_scan, "surfaceChanged: ");
+            }
+            @Override
+            // This is called when the surface is destroyed.
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                Log.d(TAG_scan, "surfaceDestroyed: ");
+            }
+        });
+
+        // set barcode detector
+        barcode.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes =  detections.getDetectedItems();
+                if(barcodes.size() > 0){
+                    Intent intent = new Intent();
+                    intent.putExtra("barcode", barcodes.valueAt(0));
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
+            }
+        });
+
+        // settings for camera
+        cameraSource = new CameraSource.Builder(this, barcode)
+                .setFacing(CameraSource.CAMERA_FACING_BACK)
+                .setRequestedFps(24)
+                .setAutoFocusEnabled(true)
+                .setRequestedPreviewSize(1920,1024)
+                .build();
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == CAMERA_PERMISSION_RECQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
